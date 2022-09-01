5baf25afa26b518d07f6cd287f746801cc3436a6
Abner Bernal <abner_bernal@outlook.com>
2022-09-01 01:19:23 -0400

    Edit Adsense

--- src/components/SideAdsense/index.tsx
+++ src/components/SideAdsense/index.tsx
@@ -10,7 +10,10 @@ export default function SideAdsense() {
   const loadAds = () => {
     try {
       if (typeof window !== "undefined") {
-        (window.adsbygoogle = window.adsbygoogle || []).push({});
+        (window.adsbygoogle = window.adsbygoogle || []).push({
+          google_ad_client: "5224230132",
+          enable_page_level_ads: true
+        });
       }
     } catch (error: any) {
       console.log("adsense error", error.message);
